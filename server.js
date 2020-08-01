270ab8f9ceb71d3394a7b55ce7884555eaed63b7
Shadee Merhi <shadmerhi@gmail.com>
2020-07-31 22:20:17 -0600

    Added initial POST and GET requests

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-  
 // Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
+const bodyParser = require('body-parser');
+const { userInfo } = require('os');
 /* Middleware*/
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+const port = 4000;
+const server = app.listen(port, listening);
+
+function listening () {
+    console.log('server running');
+    console.log(`running on local host: ${port}`);
+}
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Spin up the server
 // Callback to debug
 
 // Initialize all route with a callback function
+app.get('/all', sendData);
 
 // Callback function to complete GET '/all'
+function sendData (request, response) {
+    response.send(projectData);
+}
 
 // Post Route
+app.post('/addData', addData);
+
+function addData(request, response) {
+    let newEntry = {
+        temperature = request.body.temperature,
+        date = request.body.date,
+        userRes = request.body.userRes
+    }
+    projectData.push(newEntry);
+}
--- website/app.js
+++ website/app.js
@@ -1,8 +1,15 @@
 // Personal API Key for OpenWeatherMap API
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '&appid=02eeb440d2c88907ac4d58e8cc4b16a4';
 
 // Event listener to add function to existing HTML DOM element
+const inputButton = document.getElementById('city');
+inputButton.addEventListener('click', performAction);
 
 /* Function called by event listener */
+function performAction () {
+    
+}
 
 /* Function to GET Web API Data*/
 
